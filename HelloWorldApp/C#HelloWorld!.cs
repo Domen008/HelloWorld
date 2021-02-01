667e08c25e413f0fe2502b3aad40cb3af5a7fb71
Domen Koder <74912929+Domen008@users.noreply.github.com>
2021-02-01 07:46:55 +0100

    Added code
    Added hello world code with a lot of comments

---
+++ HelloWorldApp/C#HelloWorld!.cs
@@ -0,0 +1,23 @@
+﻿// C# program to print Hello World! 
+using System;
+
+// namespace declaration 
+namespace HelloWorldApp
+{
+
+    // Class declaration 
+    class Program
+    {
+        // Main Method 
+        static void Main(string[] args)
+        {
+            // statement 
+            // writing out (printing) Hello World! 
+            Console.WriteLine("Hello World!");
+
+            // Prevents the screen from  
+            // opening and closing too quickly
+            Console.ReadLine();
+        }
+    }
+}
